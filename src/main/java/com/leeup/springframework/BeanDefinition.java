fb7dbe2a7bf5958b10345241a6cb9856fa91f7e3
cLeeX <lcup@foxmail.com>
2022-11-05 16:24:22 +0800

    feat(spring): initial project.

---
+++ src/main/java/com/leeup/springframework/BeanDefinition.java
@@ -0,0 +1,14 @@
+package com.leeup.springframework;
+
+import lombok.Data;
+
+/**
+ * @author clee
+ * @description Bean定义
+ * @date 2022年11月5日 16:16
+ **/
+@Data
+public class BeanDefinition {
+
+    private Object bean;
+}
